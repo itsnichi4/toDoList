fcf2d46f40c1a6fa2ae6cce25df73f12ad8cce9b
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-03-02 21:52:19 +0200

    module exports

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+
+      {
+        test: /\.pico.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  },
+  watch: true
+}
+
